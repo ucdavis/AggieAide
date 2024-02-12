ace0bfbdf69bbebb22de1ee5ee93e55dc9e052ed
Scott Kirkland <srkirkland@gmail.com>
2024-02-12 15:15:11 -0800

    rewriting vector storage for new JSON doc source

--- src/vectorize.ts
+++ src/vectorize.ts
@@ -6,10 +6,11 @@ import {
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { Document } from 'langchain/document';
-import ExcelJS from 'exceljs';
-import { compile } from 'html-to-text';
+import dotenv from 'dotenv';
+import * as fs from 'fs';
+import { IndicesUpdateAliasesAction } from '@elastic/elasticsearch/lib/api/types';
 
-const compiledConvert = compile(); // options could be passed here
+dotenv.config();
 
 // let's try to do full RAG with OpenAI assistants & ElasticSearch
 const apiKey = process.env.OPENAI_API_KEY; // Replace with your API key or use environment variable
@@ -34,25 +35,79 @@ const config: ClientOptions = {
   },
 };
 
+// we are going to store in a versioned index and then update the alias
+const elasticIndexAlias = process.env.ELASTIC_INDEX ?? 'aggieaide_vectorstore';
+const realIndexName = `aggieaide_vectorstore_${Date.now()}`;
+
 const clientArgs: ElasticClientArgs = {
   client: new Client(config),
-  indexName: process.env.ELASTIC_INDEX ?? 'kb_vectorstore',
+  indexName: realIndexName,
   vectorSearchOptions: {
     similarity: 'cosine', // since this is what openAI uses
   },
 };
 
+const createAndUpdateAlias = async () => {
+  try {
+    // Step 0: Assume the index is already created, since langchain does that for us
+
+    // Find all indices the alias currently points to
+    const aliasExists = await clientArgs.client.indices.existsAlias({
+      name: elasticIndexAlias,
+    });
+
+    const actions: IndicesUpdateAliasesAction[] = [
+      { add: { index: realIndexName, alias: elasticIndexAlias } },
+    ];
+
+    const indexesToRemove: string[] = [];
+
+    // if we already have an alias, remove it from all the indexes it points to
+    // and then delete the old indexes
+    if (aliasExists) {
+      const currentIndices = await clientArgs.client.cat.aliases({
+        name: elasticIndexAlias,
+        format: 'json',
+      });
+
+      for (const index of currentIndices) {
+        if (index.index) {
+          indexesToRemove.push(index.index);
+        }
+        actions.push({
+          remove: { index: index.index, alias: elasticIndexAlias },
+        });
+      }
+    }
+
+    // Execute -- update the alias (remove from old indices and add to new index)
+    await clientArgs.client.indices.updateAliases({
+      body: { actions },
+    });
+
+    console.log(
+      `Alias ${elasticIndexAlias} is now pointing to ${realIndexName}`,
+    );
+
+    // delete the old indexes
+    for (const index of indexesToRemove) {
+      await clientArgs.client.indices.delete({ index });
+      console.log(`Deleted old index: ${index}`);
+    }
+  } catch (error) {
+    console.error(`Failed to create/update alias:`, error);
+  }
+};
+
 // const vectorStore = new ElasticVectorSearch(embeddings, clientArgs);
 const processIntoDocuments = async (documents: KbDocument[]) => {
   const processedDocuments = documents.map((document) => {
-    const text = compiledConvert(document.htmlContent);
-
     // append the title to the top of the text since it might be helpful in searching
-    const textWithTitle = `${document.title} ${text}`;
+    const textWithTitle = `${document.title} ${document.content}`;
 
     return new Document({
       pageContent: textWithTitle,
-      metadata: { id: document.id, title: document.title },
+      metadata: { id: document.id, title: document.title, url: document.url },
     });
   });
 
@@ -74,47 +129,38 @@ const processIntoDocuments = async (documents: KbDocument[]) => {
     batchedSplitDocs.push(splitDocs.slice(i, i + 200));
   }
 
-  // store the docs in batches
+  // store the docs in batches in our tmp index
   for (const batch of batchedSplitDocs) {
     console.log('storing batch size: ', batch.length);
     await ElasticVectorSearch.fromDocuments(batch, embeddings, clientArgs);
   }
 
+  // now that we have stored all the documents, we can update the alias and delete the old index
+  await createAndUpdateAlias();
+
+  // we now have a new index with the documents stored and our alias points to it
   console.log('storage complete');
 };
 
-// get back the rows from the excel file
-const processExcel = async (path: string) => {
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile(path);
-  const sheet = workbook.getWorksheet(1);
+// read all kb documents from the data folder
+const loadKbDocuments = async (path: string) => {
+  const files = await fs.promises.readdir(path);
 
-  if (!sheet) {
-    throw new Error('Sheet not found');
+  const docs: KbDocument[] = [];
+
+  for (const file of files) {
+    const fileContents = await fs.promises.readFile(`${path}/${file}`, 'utf8');
+
+    const doc = JSON.parse(fileContents);
+
+    docs.push(doc);
   }
 
-  const documents: KbDocument[] = [];
-
-  sheet.eachRow(async (row) => {
-    const id = row.getCell('A').value?.toString() ?? '';
-    const title = row.getCell('E').value?.toString() ?? '';
-    const htmlContent = row.getCell('J').value?.toString() ?? '';
-
-    documents.push({
-      id,
-      title,
-      htmlContent,
-      text: '',
-    });
-  });
-
-  return documents;
+  return docs;
 };
 
 const main = async () => {
-  const docs = await processExcel(
-    '/Users/postit/Documents/projects/kb-bot/docs/kb_knowledge.xlsx',
-  );
+  const docs = await loadKbDocuments('./data');
 
   console.log('doc count: ', docs.length);
 
@@ -131,8 +177,9 @@ main()
   });
 
 interface KbDocument {
+  sys_id: string;
   id: string;
   title: string;
-  htmlContent: string;
-  text: string;
+  url: string;
+  content: string;
 }
